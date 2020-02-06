1e1934e7e9f9623b64a297850a9b83a280a290be
nikhil19rathore99 <nikhilrathoreb4i@gmail.com>
2020-02-06 22:08:57 +0530

    Debit_Card_Generator_UI

---
+++ android/app/src/main/kotlin/com/example/debit_card_generator_prac_day_03/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.debit_card_generator_prac_day_03
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
